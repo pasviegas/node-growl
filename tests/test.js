87bfef38585a3c5a6cd92efb7c6f10e470e5042a
Pedro Antonio Viegas <Pedro@Jarvis.local>
2010-01-18 17:50:21 -0200

    bug fix

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,4 @@
 var Growl = require('../lib/growl').Growl;
 
-Growl.notify('5 passes', { name: 'jspec' })
-Growl.notify('5 passes', { image: 'pass.icns' })
-Growl.notify('5 passes', { image: 'Safari' })
+
+Growl.notify('Lucas Bicha', { name: "tets sda",title: 'Verdades.. wdsad.', image: 'Gmail' })
