86f6924c723ae29951df01bc78c3532aebe79c92
Pedro Antonio Viegas <Pedro@Jarvis.local>
2010-01-30 11:55:54 -0200

    now working

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,7 @@
 var Growl = require('../lib/growl').Growl;
 
 
-Growl.notify('Lucas Bicha', { name: "tets sda",title: 'Verdades.. wdsad.', image: 'Gmail' })
+Growl.notify('You have mail!')
+Growl.notify('5 new messages', { sticky: true })
+Growl.notify('5 new emails', { title: 'Email Client', image: 'Safari', sticky: true })
+
